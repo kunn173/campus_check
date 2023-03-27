e45010b8fa784346ae95470e5dd893393a4ec9fe
Haorui Su <2774086S@student.gla.ac.uk>
2023-03-28 05:01:16 +0800

    final version

--- campus/admin.py
+++ campus/admin.py
@@ -4,11 +4,35 @@ from campus.models import Location, Degree, University, Course, User, Enrollment
 admin.site.register(Location)
 admin.site.register(Degree)
 admin.site.register(Enrollment)
-admin.site.register(Review)
 
 # Add in this class to customise the Admin Interface
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
+    def get_queryset(self, request):
+        qs = super(CategoryAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(name=request.user.username.replace("_", " "))
 # Update the registration to include this customised interface
+
+
+# class CourseAdmin(admin.ModelAdmin):
+#     def get_queryset(self, request):
+#         qs = super(CourseAdmin, self).get_queryset(request)
+#         if request.user.is_superuser:
+#             return qs
+#         return qs.filter(university=request.user.username.replace("_", " "))
+
+# class ReviewAdmin(admin.ModelAdmin):
+#     def get_queryset(self, request):
+#         qs = super(ReviewAdmin, self).get_queryset(request)
+#         if request.user.is_superuser:
+#             return qs
+#         return qs.filter(university=request.user.username.replace("_", " "))
+
+
 admin.site.register(University, CategoryAdmin)
-admin.site.register(Course, CategoryAdmin)
+# admin.site.register(Course, CourseAdmin)
+# admin.site.register(Review, ReviewAdmin)
+admin.site.register(Course)
+admin.site.register(Review)
